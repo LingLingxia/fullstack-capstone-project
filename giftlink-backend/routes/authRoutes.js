9f3db9f98a7682e31bbd49c574387ee3fdbbbd6e
LingLingxia <llxlansing@qq.com>
2024-07-09 20:08:33 -0700

    add auth logic

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const app = express();
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+// Task 1: Use the `body`,`validationResult` from `express-validator` for input validation
 const { body, validationResult } = require('express-validator');
 const connectToDatabase = require('../models/db');
 const router = express.Router();
@@ -9,19 +9,23 @@ const dotenv = require('dotenv');
 const pino = require('pino'); //a log tool
 
 const logger = pino(); 
+dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET;
 
 router.post('/register', async (req, res) => {
     try {
-        // Task 1: Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`
+        // Task 1: Connect to `users` in MongoDB through `connectToDatabase` in `db.js`
         const db = await connectToDatabase();
 
         // Task 2: Access MongoDB collection
-        const collection = db.collection("gifts");
+        const collection = db.collection("users");
 
         //Task 3: Check for existing email
         const existingEmail = await collection.findOne({ email: req.body.email });
-        //todo: do something when email existing
+        if (existingEmail) {
+            logger.error('Email id already exists');
+            return res.status(400).json({ error: 'Email id already exists' });
+        }
 
         const salt = await bcryptjs.genSalt(10);
         const hash = await bcryptjs.hash(req.body.password, salt);
@@ -92,3 +96,54 @@ router.post('/login', async (req, res) => {
 
     }
 });
+
+
+
+
+router.put('/update', async (req, res) => {
+    // Task 2: Validate the input using `validationResult` and return approiate message if there is an error.
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        logger.error("validation errors in update request",errors.array());
+        return res.status(400).json({errors:errors.array()});
+    }
+    try {
+        // Task 3: Check if `email` is present in the header and throw an appropriate error message if not present.
+        const email = req.headers.email;
+        if(!email){
+            logger.error("Email not found in the request headers");
+            return res.status(400).json({error:"Email not found in the request headers"});
+        }
+        // Task 4: Connect to MongoDB
+        const db = await connectToDatabase();
+        const collection = db.collection("users");
+        // Task 5: find user credentials in database
+        const existingUser = await collection.findOne({email});
+        if (!existingUser) {
+            logger.error('User not found');
+            return res.status(404).json({ error: "User not found" });
+        }
+        existingUser.firstName = req.body.name;
+        existingUser.updatedAt = new Date();
+
+        // Task 6: update user credentials in database
+        const updateUser = await collection.findOneAndUpdate(
+            {email},
+            {$set:existingUser},
+            {returnDocument:'after'}
+        );
+        // Task 7: create JWT authentication using secret key from .env file
+        const payload = {
+            user:{
+                id:updateUser._id.toString(),
+            },
+        };
+        const authtoken = jwt.sign(payload,JWT_SECRET);
+        res.json({authtoken});
+    } catch (e) {
+        return res.status(500).send('Internal server error');
+
+    }
+});
+
+module.exports = router;
